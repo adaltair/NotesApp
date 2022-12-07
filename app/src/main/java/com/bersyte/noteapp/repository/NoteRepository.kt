2c3f448e4f1e3c2c885a65c7389e0d741c54cda7
Altair Adebietov <83090396+adaltair@users.noreply.github.com>
2022-12-07 16:09:23 +0600

    finally

--- app/src/main/java/com/bersyte/noteapp/repository/NoteRepository.kt
+++ app/src/main/java/com/bersyte/noteapp/repository/NoteRepository.kt
@@ -1,14 +1,13 @@
 package com.bersyte.noteapp.repository
 
-import android.provider.ContactsContract
 import com.bersyte.noteapp.db.NoteDatabase
 import com.bersyte.noteapp.model.Note
 
 class NoteRepository(private val db: NoteDatabase) {
 
-    suspend fun insertNote(note: ContactsContract.CommonDataKinds.Note) = db.getNoteDao().insertNote(note)
-    suspend fun deleteNote(note: ContactsContract.CommonDataKinds.Note) = db.getNoteDao().deleteNote(note)
-    suspend fun updateNote(note: ContactsContract.CommonDataKinds.Note) = db.getNoteDao().updateNote(note)
+    suspend fun insertNote(note: Note) = db.getNoteDao().insertNote(note)
+    suspend fun deleteNote(note: Note) = db.getNoteDao().deleteNote(note)
+    suspend fun updateNote(note: Note) = db.getNoteDao().updateNote(note)
     fun getAllNotes() = db.getNoteDao().getAllNotes()
     fun searchNote(query: String?) = db.getNoteDao().searchNote(query)
 
